fdd02b033d384731e9959ef9d293438a8cfd58b7
aysikh <amy.sikhammountry@gmail.com>
2020-12-16 17:52:06 -0500

    added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Course.destroy_all
+Lesson.destroy_all
+
+
+User.create(
+    name: "nugget", 
+    email: "nugget@gmail.com",
+    password: "nugget"
+)
+
+intro_to_prog = Course.create(
+    name: "Introduction to Programming"
+)
+
+Lesson.create(
+    title: "Defining a variable", 
+    information: "In software programming, variables are names used to hold one or more values. Instead of repeating these values in multiple places in your code, the variable holds the results of a calculation, database call, results of a database query, or other value.",
+    video_url: "TBA",
+    course_id: intro_to_prog.id
+)
