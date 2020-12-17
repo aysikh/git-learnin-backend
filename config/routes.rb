c6b8347c2448bac2f84ac387d70b62cdc09bb0a8
aysikh <amy.sikhammountry@gmail.com>
2020-12-17 12:19:06 -0500

    added devise to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :user_lessons
-  resources :labs
-  resources :lessons
-  resources :enrollments
-  resources :courses
-  resources :users
+    devise_for :users, :controllers => { registrations: 'registrations'}
+    resources :user_lessons
+    resources :labs
+    resources :lessons
+    resources :enrollments
+    resources :courses
+    resources :users
 
-  post "/login", to: "users#login", as: "login"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    post "/login", to: "users#login", as: "login"
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
